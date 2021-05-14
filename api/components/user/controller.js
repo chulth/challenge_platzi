4cb7e7c90b36265c24368ad810c9db9817e8e8b4
John Bulla <chulth@gmail.com>
2021-05-13 23:23:22 -0300

    Firts commit

---
+++ api/components/user/controller.js
@@ -0,0 +1,15 @@
+
+module.exports = function(injectedStore){
+    let store = injectedStore;
+    if (!store){
+        store = require('../../../store/dummy');
+
+    }
+    
+    function list(){
+        return store.list(TABLA);
+    }
+    return{
+        list,
+    };
+}
