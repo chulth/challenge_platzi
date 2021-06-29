6e9e3fbdf165163e7131133349a2141ca1a453ed
John Bulla <chulth@gmail.com>
2021-06-28 21:30:50 -0300

    add categories

--- store/categories.js
+++ store/categories.js
@@ -2,18 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-    createdAt: {
-        type: Date, default: Date.now 
-    },
-    updatedAt: {
-        type: Date
-    },
-    name: {
-        type: String
-    },
-    image: {
-        type: String
-    }
+    createdAt: {type: Date, default: Date.now },
+    updatedAt: {type: Date},
+    name: {type: String},
+    image: {type: String}
 
 });
 
